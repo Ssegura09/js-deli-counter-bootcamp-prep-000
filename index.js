090c631d6ebf4258643716c8c873dcb9d35e3e6f
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-06-18 14:15:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
+  katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
+  if (katzDeliLine.length > 0)
+  return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!"
+  
 }
+
 function currentLine(katzDeliLine){
   var string = `The line is currently: `;
   var newLine = [];
@@ -18,12 +21,6 @@ function currentLine(katzDeliLine){
     return string;
   }
 }
-
-
-
-
-
-
 
 
 
