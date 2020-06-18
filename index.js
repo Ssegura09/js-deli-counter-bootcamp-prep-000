a196afb50bb84c49e0e3260e6c056214f175979e
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-06-18 15:24:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function currentLine(katzDeliLine){
     for (let i=0; i < katzDeliLine.length; i++){
       newLine[i] = `${[i+1]}. ${katzDeliLine[i]}`;
     }
-    string += newLine.join(', ')
-    return string;
+    
   }
 }
 
